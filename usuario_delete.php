288620728e265efbe7435c59cd284cb5cfc2d672
Maykon Douglas <maykon2000douglas@gmail.com>
2019-11-16 13:52:46 -0300

    Painel do usuário adicionado

---
+++ usuario_delete.php
@@ -0,0 +1,15 @@
+<?php
+require "verify_session.php";
+require('./php/verify_session_admin.php');
+include "config.php";
+
+$uid = $_GET['pos'];
+$query = "DELETE FROM usuario WHERE uid = '$uid'";
+if (mysqli_query($con, $query)){
+  header("Location: usuarios.php");
+} else {
+  $error = "Não foi possível excluir o usuário!";
+  $locale = "usuarios.php";
+  header("Location: error.php?error_msg=$error&locale=$locale");
+}
+ ?>
